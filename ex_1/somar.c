89a8cd23ad12ee8797075ed24c377fdf095e3327
Alysson <4ly.alcantara@gmail.com>
2021-08-23 12:14:05 -0300

    Update somar.c

--- ex_1/somar.c
+++ ex_1/somar.c
@@ -2,31 +2,39 @@
 #include <locale.h>
 #include <stdlib.h>
 
-int escolha = 1;
+int loop = 1;
+
+void clear_screen(){
+	#ifdef _WIN32 
+		#ifdef _WIN64
+	        system("cls");
+	    #else 
+	        system("cls");
+	    #endif
+	#else
+	    system("clear");
+	#endif
+}
 
 void menu()
 {
 	printf(" -- Fim da execução -- \n");
 	printf("\n1 - continuar\n0 - sair\n\n| ");
-	scanf("%d", &escolha);
-#ifdef OS_Windows
-	system("cls");
-#else
-	system("clear");
-#endif
+	scanf("%d", &loop);
+	clear_screen();
 }
 
 int main()
 {
-	float num[2];
+	float number[2];
 	setlocale(LC_ALL, "portuguese");
 	do
 	{
 		printf("\nDigite um número para ser somado: ");
-		scanf("%f", &num[0]);
+		scanf("%f", &number[0]);
 		printf("Digite o próximo número: ");
-		scanf("%f", &num[1]);
-		printf("\n\nA soma dos dois números é: %.3f\n\n\n\n", num[0] + num[1]);
+		scanf("%f", &number[1]);
+		printf("\n\nA soma dos dois números é: %.3f\n\n\n\n", number[0] + number[1]);
 		menu();
-	} while (escolha >= 1);
+	} while (loop >= 1);
 }
