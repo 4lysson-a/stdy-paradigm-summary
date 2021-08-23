b79b941eb319c198efbf2f7c7b99a7a82ab882c5
4lysson-a <4ly.alcantara@gmail.com>
2021-08-23 11:52:17 -0300

    fixing os system errors

--- ex_1/somar.c
+++ ex_1/somar.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
+#include <stdlib.h>
 
 int escolha = 1;
 
-void menu(){
-	printf("Digite\n");
+void menu()
+{
+	printf(" -- Fim da execução -- \n");
 	printf("\n1 - continuar\n0 - sair\n\n| ");
 	scanf("%d", &escolha);
+#ifdef OS_Windows
+	system("cls");
+#else
+	system("clear");
+#endif
 }
 
-int main(){
+int main()
+{
 	float num[2];
 	setlocale(LC_ALL, "portuguese");
 	do
 	{
-		system("cls");
-		system("clear");
 		printf("\nDigite um número para ser somado: ");
 		scanf("%f", &num[0]);
 		printf("Digite o próximo número: ");
 		scanf("%f", &num[1]);
-		printf("\n\nA soma dos dois números é: %.3f\n\n\n\n", num[0]+num[1]);
+		printf("\n\nA soma dos dois números é: %.3f\n\n\n\n", num[0] + num[1]);
 		menu();
-	} while (escolha == 1);
-	
+	} while (escolha >= 1);
 }
