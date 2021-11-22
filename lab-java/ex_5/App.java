7dafa7ac221b62d7ad05afa56c9795e2016e2cd6
4lysson-a <4ly.alcantara@gmail.com>
2021-11-22 12:11:19 -0300

    add ex

---
+++ lab-java/ex_5/App.java
@@ -0,0 +1,31 @@
+
+// Escreva um programa JAVA que calcule a média de quatro notas. O programa deve mostrar o valor da média e indicar se foi aprovado ou reprovado
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    double[] nota = new double[4];
+
+    System.out.print("\n");
+    for (int i = 0; i < nota.length; i++) {
+      System.out.print("Digite a nota " + (i + 1) + ": ");
+      nota[i] = new Scanner(System.in).nextDouble();
+    }
+
+    // Calculo média
+    double media = 0;
+    for (int i = 0; i < nota.length; i++) {
+      media += nota[i];
+    }
+    media /= nota.length;
+
+    System.out.print("\nmedia: " + media + "\n");
+
+    if (media >= 6) {
+      System.out.println("\nAprovado :>\n");
+    } else {
+      System.out.println("\nReprovado :<\n");
+    }
+
+  }
+}
